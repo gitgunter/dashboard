8791c3a227dd0ca472562720aaf89a835c133b80
Keylor <keylordani@gmail.com>
2024-04-25 02:31:54 -0600

    Empty commit

--- src/pages/Resumen/Resumen.jsx
+++ src/pages/Resumen/Resumen.jsx
@@ -43,7 +43,6 @@ function Resumen() {
           <Breadcrumbs paths={breadcrumbs} />
           <div className={css.headerWrapper}>
             <div className={css.titleAndCaption}>
-              Something new! 🚀
               <WelcomeMessage username={isLoading ? '--' : data.username} />
               <p className={css.caption}>Resumen de tu progreso por Teodrive</p>
             </div>
